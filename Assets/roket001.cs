95c7827ebd162d6a7195100682f822f160a25166
gitmsx <gitmsx@mail.ru>
2022-11-12 10:33:29 +0300

    Saved on 12.11.2022-10:33:19,29

---
+++ Assets/roket001.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class roket001 : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    [SerializeField] private float speed_1 = 11.0f;
+    private float SpeedRocket = 10.0f;
+    private float DeltaSpeedRocket = 0.1123f;
+    private bool SpeedChanged = true;
+    private int multip = 1;
+
+    public Text Text1;
+    public GameObject Text2;
+
+    private Text Text12;
+    void Start()
+    {
+        Text2 = GameObject.Find("Text");
+        //  Text2.transform.Translate(1,1,1);
+        Text12 = Text2.GetComponent<Text>();
+        Text12.text = "111111111";
+    }
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        //        rb.angularDrag = 2;
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject gameObject22;
+        gameObject22=collision.gameObject;
+
+
+        switch (gameObject22.tag)
+        {
+
+            case "enemy":
+
+                Debug.Log("Enemy");
+                Console.WriteLine("Enemy");
+                break;
+            case "base":
+
+                Debug.Log("base");
+                Console.WriteLine("base");
+                break;
+
+
+
+
+            default:
+                break;
+        }
+
+
+    }
+
+    void Sstabil()
+    {
+        Vector3 pos55 = transform.position;
+        transform.position = new Vector3(0, pos55.y, 0);
+
+    }
+    void Sstabil2()
+    {
+        Vector3 pos55 = transform.position;
+        transform.position = new Vector3(Mathf.Round(pos55.x), pos55.y, Mathf.Round(pos55.z));
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetAxis("Mouse ScrollWheel")!=0)
+        {
+            multip += Mathf.FloorToInt(Input.GetAxis("Mouse ScrollWheel") * 10);
+            SpeedChanged = true;
+
+        }
+
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            SpeedRocket += DeltaSpeedRocket;
+            SpeedChanged = true;
+            //    Sstabil();
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            SpeedRocket -= DeltaSpeedRocket;
+            SpeedChanged = true;
+            //    Sstabil();
+        }
+        if (SpeedChanged == true)
+        {
+
+
+            Text12.text = "Импульс "+Math.Round(SpeedRocket, digits: 3).ToString() + "  Mult " + Math.Round(multip, digits: 3).ToString();
+            SpeedChanged = false;
+
+        }
+        {
+            rb.AddRelativeForce(0, SpeedRocket * multip * Time.deltaTime, 0, ForceMode.Impulse);
+
+        }
+
+
+        float rotation_1 = Input.GetAxis("Horizontal") * speed_1 * Time.deltaTime;
+
+        rb.AddRelativeTorque(rotation_1, 0, 0, ForceMode.Impulse);
+
+
+    }
+}
